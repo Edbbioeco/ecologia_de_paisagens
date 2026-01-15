0f1c8eaeef908a7db9492d64d34c345958f0a8eb
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-15 10:40:34 -0300

    Scripts para analisar dados ambientais que afetam a diversidade

---
+++ Dados ambientais e diversidade.R
@@ -0,0 +1,21 @@
+# Pacotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(sf)
+
+library(terra)
+
+library(tidyterra)
+
+library(vegan)
+
+library(fields)
+
+# Dados ----
+
+## Coordenadas e matriz ----
+
+### Importando ----
